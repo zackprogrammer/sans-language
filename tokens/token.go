bdbdf302f3f2113a076dd0c68e84d947745fd153
zackprogrammer <63200308+zackprogrammer@users.noreply.github.com>
2020-11-09 17:57:01 -0500

    For some fucking reason i had to manly go on gtihub.com.
    it wouldnt send all the code to github so for some fucking reason i had to go on gigtuhb.com and make changes.

--- tokens/token.go
+++ tokens/token.go
@@ -3,3 +3,15 @@ package tokens
 // Type is a token type.
 type Type string
 
+const (
+	ECHO = "ECHO"
+)
+
+type token struct{
+	Type Type
+	Literal string
+}
+
+var keywords = map[string]Type{
+	"echo": ECHO
+}
